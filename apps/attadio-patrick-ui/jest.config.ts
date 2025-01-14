29715c89badc13414c4767559954e613009da43d
Patrick Attadio <patrick.attadio@gmail.com>
2025-01-14 14:54:03 +0100

    Initial commit
    To connect your workspace to Nx Cloud, push your repository
    to your git hosting provider and go to the following URL:
    
    https://cloud.nx.app/connect/mcm87RpMcO

---
+++ apps/attadio-patrick-ui/jest.config.ts
@@ -0,0 +1,10 @@
+export default {
+  displayName: 'attadio-patrick-ui',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
+    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/react/babel'] }],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/attadio-patrick-ui',
+};
