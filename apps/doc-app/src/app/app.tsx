33b3edba51175acf1ea7a2253d0a7b901758736d
Patrick Attadio <patrick.attadio@gmail.com>
2025-01-30 16:49:17 +0100

    add publish_packages.yaml

--- apps/doc-app/src/app/app.tsx
+++ apps/doc-app/src/app/app.tsx
@@ -1,12 +1,11 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from './app.module.css';
-
-import NxWelcome from './nx-welcome';
+import { Button } from '@attadio-patrick-ui/button';
 
 export function App() {
   return (
     <div>
-      <NxWelcome title="doc-app" />
+      <Button label='Cliccami' className='color' onClick={() => console.log('Cliccato!')} />
     </div>
   );
 }
