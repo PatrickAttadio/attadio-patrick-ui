ee9559876b98e8e6c476ca5ab1e18550395138d8
Patrick Attadio <patrick.attadio@gmail.com>
2025-02-04 12:51:18 +0100

    hero e header

--- apps/doc-app/src/app/app.tsx
+++ apps/doc-app/src/app/app.tsx
@@ -1,11 +1,13 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from './app.module.css';
 import { Button } from '@attadio-patrick-ui/button';
+import { Header } from '@attadio-patrick-ui/header';
 
 export function App() {
   return (
     <div>
-      <Button label='Cliccami' className='color' onClick={() => console.log('Cliccato!')} />
+      <Header />
+      {/* <Button label='Cliccami' className='color' onClick={() => console.log('Cliccato!')} /> */}
     </div>
   );
 }
---
+++ libraries/header/src/lib/header.tsx
@@ -0,0 +1,34 @@
+import styles from './header.module.css';
+import { useState } from 'react';
+
+export function Header() {
+  const [isComponentsMenuOpen, setIsComponentsMenuOpen] = useState(false);
+
+  const toggleComponentsMenu = () => {
+    setIsComponentsMenuOpen(!isComponentsMenuOpen);
+  };
+
+  return (
+    <div className={styles['container']}>
+      <div className={styles['header']}>
+        <h1>ATTADIO-UI</h1>
+        <nav>
+          <ul>
+        <li>Getting Start</li>
+        <li onClick={toggleComponentsMenu}>
+          Components
+          <span className={styles['dropdown-icon']}>▼</span>
+          {isComponentsMenuOpen && (
+            <ul>
+          <li>Button</li>
+          {/* <li>Card</li> */}
+            </ul>
+          )}
+        </li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+  );
+}
+
